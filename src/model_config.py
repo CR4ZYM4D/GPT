076f32f94fc8b7f4caab263404b1904f7e5ec702
CR4ZYM4D <madhavdixit482@gmail.com>
2025-07-15 23:51:26 +0530

    decreasing batch size

--- src/model_config.py
+++ src/model_config.py
@@ -3,7 +3,7 @@ class ModelBlockConfig():
 
     def __init__(self, 
                  embedding_dimension: int = 1024, 
-                 batch_size: int = 32,
+                 batch_size: int = 16,
                  max_sequence_length: int = 2048, 
                  num_heads: int = 8, 
                  dropout_fraction: float = 0.4):
