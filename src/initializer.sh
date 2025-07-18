130aaf878e19555190eee3ab767a5f2a4ef19294
CR4ZYM4D <madhavdixit482@gmail.com>
2025-07-18 08:12:48 +0530

    remote init script

---
+++ src/initializer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+apt update
+
+apt upgrade -y
+
+apt install unzip -y
+
+nvidia-smi
+
+good =$(python3 -c "import torch; print(torch.cuda.is_available())")
+
+if [[ "$good" == "True" ]]; then
+
+    echo "Pytorch installation is good to go"
+
+    echo "torch version: $(python3 -c 'import torch; print(torch.__version__)')"
+
+    exit 0
+
+else 
+
+    pip3 uninstall torch -y
+ 
+    pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128 --force-reinstall
+
+    echo "re-installed torch" 
+
+    exit 0
+
+fi
