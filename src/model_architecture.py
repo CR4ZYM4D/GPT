d83821524c62acef92784854dcd085a955b1fe8f
CR4ZYM4D <madhavdixit482@gmail.com>
2025-07-21 23:22:25 +0530

    fixing deepspeed error

--- src/model_architecture.py
+++ src/model_architecture.py
@@ -7,11 +7,13 @@ from transformers import AutoTokenizer
 from model_config import GPT1ModelConfig, GPT2ModelConfig, ModelBlockConfig
 from model_components import DecoderBlock, TokenEmbeddings, PositionalEncodings, LayerNorm
 
+import os
+
 default_block_config = ModelBlockConfig()
 
 default_tokenizer = AutoTokenizer.from_pretrained("./gpt/models/tokenizer")
 
-version = (int)(input("enter model config version (0 for small, any other integer for large): "))
+version = int(os.environ.get("MODEL_CONFIG_VERSION", 0))  # default GPT-1
 
 default_model_config = GPT1ModelConfig(default_block_config, default_tokenizer) if version == 0 else GPT2ModelConfig(default_block_config, default_tokenizer)
 
